bdb40b308801619ea3cfb068025421186bd2ab7e
Navdeep Nagar <Ankit@SANGEETAs-MacBook-Air.local>
2024-02-20 17:18:59 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import "@styles/globals.css";
+
+import Nav from "../components/Nav";
+import Provider from "../components/Provider";
+import { Suspense } from 'react'
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <Suspense>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+        </Suspense>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
